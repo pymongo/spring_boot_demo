31cb60f7423aaad5189839f231e583008d00980d
吴翱翔 <pylint@yandex.com>
2020-03-17 11:11:35 +0800

    create spring boot demo project

---
+++ src/main/java/spring/demo/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package spring.demo.controllers;
+
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+//@RestController
+public class HomeController {
+//  @RequestMapping("/")
+//  public String index() {
+//    return "Greetings from Spring Boot!";
+//  }
+}
